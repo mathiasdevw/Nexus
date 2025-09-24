14da5f8e8283360c7001a0e02086af3deaef02f2
Mathias de Abreu Carolino <mathias@MacBook-Air-de-Mathias.local>
2025-09-24 20:23:25 -0300

    cliente/repository

--- src/main/java/br/com/catolica/nexus/Controller/clienteController.java
+++ src/main/java/br/com/catolica/nexus/Controller/clienteController.java
@@ -1,1 +1,16 @@
-//teste
+package br.com.catolica.nexus.Controller;
+public class clienteController {
+
+    private String nome;
+    private String cpf;
+    private int idade;
+    private String endereco;
+
+    public clienteController(String nome, String cpf, int idade, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.endereco = endereco;
+    }
+    
+}
--- src/main/java/br/com/catolica/nexus/Model/cliente.java
+++ src/main/java/br/com/catolica/nexus/Model/cliente.java
@@ -1,1 +1,48 @@
-//teste
+
+package br.com.catolica.nexus.Model;
+
+public class cliente {
+    private String nome;
+    private String cpf;
+    private int idade;
+    private String endereco;
+
+    public cliente(String nome, String cpf, int idade, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
